c8e935c1f0d179052f5653a01ffef97ae9ddeadd
SoulFletcher <not1ikeother@mail.ru>
2024-08-13 18:07:10 +0300

    Пустое меню настроек ipfs

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -29,11 +29,9 @@ const Home: NextPage = () => {
     }
   }, [ipfsNode]);
 
-  // Обрабатываем данные только после их загрузки
   const data = projectlist
     ? projectlist.map(projectId => ({
         id: projectId,
-        // id: <ProjectTitleFromId projectId={projectId} helia={helia} heliaOnline={heliaOnline}></ProjectTitleFromId>,
       }))
     : [];
   const columns = ["id"];
@@ -53,7 +51,33 @@ const Home: NextPage = () => {
       <div className="flex flex-row items-start h-96">
         {tab === "main" && (
           <div className="card bg-base-100 shadow-xl">
-            <div className="card-body">
+            <div className="card-body relative">
+              <div className="absolute top-2 right-2">
+                <div className="dropdown dropdown-end">
+                  <label tabIndex={0} className="btn btn-ghost btn-circle">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4" />
+                    </svg>
+                  </label>
+                  <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <li>
+                      <a>Setting 1</a>
+                    </li>
+                    <li>
+                      <a>Setting 2</a>
+                    </li>
+                    <li>
+                      <a>Setting 3</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
               <p>
                 Chainlance is a decentralized freelance marketplace. Chainlance sets itself apart from other freelance
                 marketplaces by prioritizing collaboration among freelancers rather than fostering competition.
--- packages/nextjs/components/NavBarChain.tsx
+++ packages/nextjs/components/NavBarChain.tsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { SettingsMenu } from "./SettingIpfsMenu";
+
 interface NavBarChainProps {
   tab: string;
   setTab: (tab: string) => void;
@@ -5,16 +8,24 @@ interface NavBarChainProps {
 
 export const NavBarChain = ({ tab, setTab }: NavBarChainProps) => {
   return (
-    <div className="navbar shadow-md bg-base-100 m-2">
-      <button className={`btn ${tab === "main" ? "btn-primary" : "btn-ghost"}`} onClick={() => setTab("main")}>
-        Main
-      </button>
-      <button className={`btn ${tab === "worker" ? "btn-primary" : "btn-ghost"}`} onClick={() => setTab("worker")}>
-        Worker
-      </button>
-      <button className={`btn ${tab === "employer" ? "btn-primary" : "btn-ghost"}`} onClick={() => setTab("employer")}>
-        Employer
-      </button>
+    <div className="navbar shadow-md bg-base-100 m-2 flex justify-between">
+      <div>
+        <button className={`btn ${tab === "main" ? "btn-primary" : "btn-ghost"}`} onClick={() => setTab("main")}>
+          Main
+        </button>
+        <button className={`btn ${tab === "worker" ? "btn-primary" : "btn-ghost"}`} onClick={() => setTab("worker")}>
+          Worker
+        </button>
+        <button
+          className={`btn ${tab === "employer" ? "btn-primary" : "btn-ghost"}`}
+          onClick={() => setTab("employer")}
+        >
+          Employer
+        </button>
+      </div>
+      <div className="flex-none">
+        <SettingsMenu />
+      </div>
     </div>
   );
 };
